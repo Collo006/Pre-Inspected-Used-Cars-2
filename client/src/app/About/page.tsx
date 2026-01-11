5c0b255abe82d2df93b4dc050fa97e411702d19e
Collins Kipruto <collinskipruto036@gmail.com>
2026-01-11 18:22:35 +0300

    submit again

--- client/src/app/About/page.tsx
+++ client/src/app/About/page.tsx
@@ -31,24 +31,24 @@ export default function About(){
                 <p  className={`sm:w-screen w-full sm:text-md text-sm ${sairaFont.className} pt-10 pl-1 pr-2 pb-10 text-navyBlue`}>There was something special about this little creature. Donna couldn&apos;t quite pinpoint what it was, but she knew with all her heart that it was true.It wasn&apos;t a matter of if she was going to try and save it, but a matter of how she was going to save it.&quot;There was something special about this little creature. Donna couldn&apos;t quite pinpoint what it was, but she knew with all her heart that it was true.It wasn&apos;t a matter of if she was going to try and save it, but a matter of how she was going to save it.&quot;</p>
             </div> 
             <div className={`bg-lightSky ml-0 w-screen rounded-xl mt-10 `}>
-               <div className={`relative mx-auto w-full min-w-[400px] h-[700px]`}>
+               <div className={`relative sm:mx-auto sm:w-full sm:min-w-[400px] sm:h-[700px] h-[200px]`}>
                 <Image src="/images/peopel.jpg" alt="Background" className={` object-cover `} fill />
                 </div> 
                 </div> 
-                <div className={`ml-1 mr-1 w-screen h-[600px] rounded-xl mt-10 grid grid-cols-[1fr_2fr] gap-1`}>
+                <div className={`sm:ml-1 sm:mr-1 w-screen sm:h-[600px]  sm:rounded-xl mt-10 sm:grid sm:grid-cols-[1fr_2fr] sm:gap-1 grid grid-cols-1`}>
                       <div className={` w-full `}>
-                        <div className={`relative mx-auto w-full min-w-[400px] h-[600px]`}>
-                    <Image src="/images/people.jpg" alt="Background" className={`rounded-xl object-cover `} fill />
+                        <div className={`relative sm:mx-auto w-full sm:min-w-[400px] sm:h-[600px] h-[300px]`}>
+                    <Image src="/images/people.jpg" alt="Background" className={`sm:rounded-xl object-cover `} fill />
                         </div>
 
                       </div>
 
-                      <div className={`bg-white rounded-xl`}>
-                       <h1 className={`${exoFont.className} text-3xl text-darkSky font-bold text-center pt-5`}>WHY CHOOSE PINUC</h1>
-                       <p className={`text-md ${sairaFont.className} pt-5  text-navyBlue`}>There was something special about this little creature. Donna couldn&apos;t quite pinpoint what it was,but she knew with all her heart that it was true. It wasn&apos;t a matter of if she was going to try and save it, but a matter of how she was going to save it. He sat staring at the person in the train stopped at the station going in the opposite direction. She sat staring ahead, never noticing that she was being watched. Both trains began to move and he knew that in another timeline or in another universe, they had been happy together.</p>
-                        <p className={`text-md ${sairaFont.className} pt-5  text-navyBlue`}>There was something special about this little creature. Donna couldn&apos;t quite pinpoint what it was,but she knew with all her heart that it was true. It wasn&apos;t a matter of if she was going to try and save it, but a matter of how she was going to save it. He sat staring at the person in the train stopped at the station going in the opposite direction. She sat staring ahead, never noticing that she was being watched. Both trains began to move and he knew that in another timeline or in another universe, they had been happy together.</p>
+                      <div className={`bg-white rounded-xl w-screen`}>
+                       <h1 className={`${exoFont.className} sm:text-3xl text-xl text-darkSky font-bold text-center pt-5`}>WHY CHOOSE PINUC</h1>
+                       <p className={`sm:text-md text-sm  ${sairaFont.className} pt-5  text-navyBlue`}>There was something special about this little creature. Donna couldn&apos;t quite pinpoint what it was,but she knew with all her heart that it was true. It wasn&apos;t a matter of if she was going to try and save it, but a matter of how she was going to save it. He sat staring at the person in the train stopped at the station going in the opposite direction. She sat staring ahead, never noticing that she was being watched. Both trains began to move and he knew that in another timeline or in another universe, they had been happy together.</p>
+                        <p className={`sm:text-md text-sm  ${sairaFont.className} pt-5  text-navyBlue`}>There was something special about this little creature. Donna couldn&apos;t quite pinpoint what it was,but she knew with all her heart that it was true. It wasn&apos;t a matter of if she was going to try and save it, but a matter of how she was going to save it. He sat staring at the person in the train stopped at the station going in the opposite direction. She sat staring ahead, never noticing that she was being watched. Both trains began to move and he knew that in another timeline or in another universe, they had been happy together.</p>
 
-                        <p className={`text-md ${sairaFont.className} pt-5  text-navyBlue`}>There was something special about this little creature. Donna couldn&apos;t quite pinpoint what it was,but she knew with all her heart that it was true. It wasn&apos;t a matter of if she was going to try and save it, but a matter of how she was going to save it. He sat staring at the person in the train stopped at the station going in the opposite direction. She sat staring ahead, never noticing that she was being watched. Both trains began to move and he knew that in another timeline or in another universe, they had been happy together.</p>
+                        <p className={`sm:text-md text-sm ${sairaFont.className} pt-5  text-navyBlue`}>There was something special about this little creature. Donna couldn&apos;t quite pinpoint what it was,but she knew with all her heart that it was true. It wasn&apos;t a matter of if she was going to try and save it, but a matter of how she was going to save it. He sat staring at the person in the train stopped at the station going in the opposite direction. She sat staring ahead, never noticing that she was being watched. Both trains began to move and he knew that in another timeline or in another universe, they had been happy together.</p>
 
                       </div>
                 </div>
